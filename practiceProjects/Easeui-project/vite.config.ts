eaa92664b59269ec9358432c98a07e42060b9480
Pallavi S Bhat <bhatpallavi.24@gmail.com>
2026-08-22 19:11:32 +0530

    fix easeui vercel Deployemrnt

--- practiceProjects/Easeui-project/vite.config.ts
+++ practiceProjects/Easeui-project/vite.config.ts
@@ -1,30 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
+import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "EaseUI",
-      fileName: (format) => `easeui.${format}.js`,
-    },
-    cssCodeSplit: true,
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
